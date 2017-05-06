b141d5e363b3e369792de2dc196bb95887a8cfdd
vishrutkohli <kohlivishrut@gmail.com>
2017-05-06 12:55:27 +0530

    a

--- api/views.py
+++ api/views.py
@@ -13,9 +13,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
 import requests
-from reportlab.pdfgen import canvas
-from reportlab.lib import colors
-from reportlab.platypus import Image
 from django.http import HttpResponse
 import urllib2
 from django.utils.decorators import method_decorator
