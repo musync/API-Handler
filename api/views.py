4d99db5b0eac521884e7a4dd4bacbe194d25a92a
vishrutkohli <kohlivishrut@gmail.com>
2017-05-06 19:28:48 +0530

    a

--- api/views.py
+++ api/views.py
@@ -189,7 +189,7 @@ def songs_saver(request):
 			o.SongUrl = "https://www.youtube.com/watch?v=" +  v_id
 			o.save()
 
-			q = o.playlist_set.get_or_create(Email =u.pk)
+			q = o.playlist_set.get_or_create(Email =u)
 			q.SongName = o.pk
 			q.save()
 		
